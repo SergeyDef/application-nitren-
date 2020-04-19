1b379d19112e7e8ff0bf43da172589c35b7a5d80
unknown <pas.sergei2014@yandex.ru>
2020-04-19 23:56:11 +0300

    init

--- src/CoreComponents/PhotoPost/Pic/Pic.js
+++ src/CoreComponents/PhotoPost/Pic/Pic.js
@@ -5,7 +5,7 @@ const Pic = (props) =>{
   
   return (
       <div className={s.pic}>
-        <img src="https://i.pinimg.com/736x/b3/5a/e5/b35ae54dc58f7fe18d883b7021742cfb--necronomicon-cthulu.jpg" alt="" />
+        <img src="https://avatars.mds.yandex.net/get-pdb/1924971/a05d7439-5109-41cd-9f5d-f4a0a418da63/s1200?webp=false" alt="" />
       </div>
     );
 }
